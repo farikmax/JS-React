400e22dd07bdc7ce0b9a9026060f68ad7e22b812
Maksym Farasiuk <farikmaxx@gmail.com>
2022-10-16 12:41:22 +0300

    add: objects & destruction objects

---
+++ 10-Objects-destruction-of-objects/script.js
@@ -0,0 +1,44 @@
+'use strict';
+
+// const obj = new Object();
+
+const options = {
+	name: 'test',
+	width: 1024,
+	height: 1024,
+	colors: {
+		border: 'black',
+		bg: 'red',
+	},
+	makeTest: function () {
+		console.log('Test');
+	},
+};
+
+options.makeTest();
+// console.log(options.name);
+
+// delete options.name;
+
+// console.log(options);
+
+let counter = 0;
+
+for (let key in options) {
+	if (typeof options[key] === 'object') {
+		for (let i in options[key]) {
+			console.log(`Свойства ${i} имеет значение ${options[key][i]}`);
+			counter++;
+		}
+	} else {
+		console.log(`Свойства ${key} имеет значение ${options[key]}`);
+		counter++;
+	}
+}
+
+console.log(counter);
+
+console.log(Object.keys(options).length);
+
+const { border, bg } = options.colors;
+console.log(border);
